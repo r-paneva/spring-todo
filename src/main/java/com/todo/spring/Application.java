c7a460127422d7eaf1bf237bb9f62369dae9862c
Rumyana Paneva <r.paneva@gmail.com>
2018-09-07 23:09:11 +0300

    hi

---
+++ src/main/java/com/todo/spring/Application.java
@@ -0,0 +1,39 @@
+package com.todo.spring;
+
+import com.todo.spring.models.Todo;
+import com.todo.spring.repositories.base.GenericRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootApplication
+public class Application {
+
+    @Autowired
+    public Application(GenericRepository<Todo> todosRepository) {
+        List<Todo> initialTodos = Arrays.asList(
+                new Todo("go to buy flowers",
+                        "flowers for your grandmother's birthday",
+                        true),
+                new Todo("to finish my project",
+                        "finish project and upload it to monday", true),
+                new Todo("go to buy flowers",
+                        "flowers for your grandmother's birthday",
+                        true),
+                new Todo("to finish my project",
+                        "finish project and upload it to mondayasdasdasdasdasdqweqwee12312412412eqweqweqwdasdaczxc", true),
+                new Todo("to finish my project",
+                        "finish project and upload it to monday", true)
+        );
+        initialTodos
+                .forEach(todosRepository::create);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
