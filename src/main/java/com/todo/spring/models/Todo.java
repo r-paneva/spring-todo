02d45f3ff794d4c1182ddc3811d3a28f3e46c254
nekr0s <alexanderrusev@abv.bg>
2018-09-08 13:03:27 +0300

    Revert "Without double seter and geter"
    This reverts commit deec38e8c6a7dd6948a856fe205f323670cc54bc.

--- src/main/java/com/todo/spring/models/Todo.java
+++ src/main/java/com/todo/spring/models/Todo.java
@@ -44,4 +44,13 @@ public class Todo extends ModelBase{
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
 }
